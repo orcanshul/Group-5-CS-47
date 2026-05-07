d1ae89d6266178883de1cff1c710adac45d12685
Orcanshul <Anshul0402@gmail.com>
2026-05-06 21:15:00 -0700

    fix: addition of FileReader + hashing

--- FileReader.asm
+++ FileReader.asm
@@ -1,67 +1,84 @@
-.include "./cli.asm"
+.include "./cli_macro.asm"
+.include "./hash.asm"
 
 .data
-errorMsg: .asciiz "Error: Could not open file."
+prompt:     .asciiz "Enter file path: "
+errorMsg:   .asciiz "Error: Could not open file."
+output:     .asciiz "\nGenerated checksum: "
+filename:   .space 128
 fileBuffer: .space 128
+
 .text
 .globl main
 
-main: 
-	#Get filename from cli.asm
-	jal getFilename
-	move $s3,$v0
-	
-	#Open file
-	li $v0, 13		#Syscall for opening file
-	move $a0, $s3		#load file into $a0
-	li $a1, 0		#set to read only
-	li $a2, 0	
-	syscall
-	
-	bltz $v0, error	#checks if file was successfully opened, if not, branches to error
-	
-	move $s0, $v0		#Save file descriptor 
+main:
+    # Get filename from user
+    print_string(prompt)
+    read_filename
 
-read_loop:	
-	#Read File
-	li $v0, 14		#Syscall for reading file
-	move $a0, $s0		#Use saved file descriptor
-	la $a1, fileBuffer		#Storage location for bytes read
-	li $a2, 127		#Set number of bytes to be read at a time
-	syscall
-	
-	move $t0, $v0		
-	
-	beqz $t0, done
-	bltz $t0, error
-	
-	la $t1, fileBuffer		#Address of start of buffer
-	add $t1, $t1, $t0	#Go to position after the last (up to 127) bytes read
-	sb $zero, 0($t1)	#End string
-	
-	#Print to make sure contents of file were read into the buffer
-	li $v0,4		
-	la $a0, fileBuffer
-	syscall
-	
-	j read_loop
+    # Strip trailing newline left by syscall 8
+    la $t0, filename
+strip_newline:
+    lb $t1, 0($t0)
+    beq  $t1, 10, do_replace
+    beqz $t1, after_strip
+    addi $t0, $t0, 1
+    j strip_newline
+do_replace:
+    sb $zero, 0($t0)
+after_strip:
+
+    # Open file (read-only)
+    li $v0, 13
+    la $a0, filename
+    li $a1, 0
+    li $a2, 0
+    syscall
+    bltz $v0, error
+    move $s0, $v0           # save file descriptor
+
+read_loop:
+    li $v0, 14
+    move $a0, $s0
+    la $a1, fileBuffer
+    li $a2, 127
+    syscall
+    move $t0, $v0
+    beqz $t0, done
+    bltz $t0, error
+
+    la $t1, fileBuffer
+    add $t1, $t1, $t0
+    sb $zero, 0($t1)        # null-terminate chunk
+
+    li $v0, 4
+    la $a0, fileBuffer
+    syscall
+
+    j read_loop
 
 done:
-	#Close file
-	li $v0, 16		#Syscall to close file
-	move $a0, $s0		#Input file descriptor to specify which file to close
-	syscall
-	
-	exit
-	
-error: 
-	#Print error message
-	li $v0,4
-	la $a0, errorMsg
-	syscall
-	
-	#Exit
-	li $v0,10
-	syscall
-	
+    # Close file
+    li $v0, 16
+    move $a0, $s0
+    syscall
 
+    # Compute and print DJB2 hash via hash.asm
+    la  $a0, filename
+    jal hash_file           # hash -> $v0
+    move $s1, $v0           # save before print_string clobbers $v0
+
+    print_string(output)
+    print_int($s1)
+    la $a0, newline
+    li $v0, 4
+    syscall
+
+    exit
+
+error:
+    li $v0, 4
+    la $a0, errorMsg
+    syscall
+    li $v0, 10
+    syscall
